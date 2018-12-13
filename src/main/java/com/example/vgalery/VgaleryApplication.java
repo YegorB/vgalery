4a59f5b8139362d0e42d6479c6d6c10a8f5ae299
Yegor Budnichenko <y.budnichenko@gmail.com>
2018-12-13 18:39:29 +0200

    spring-boot configuration

--- src/main/java/com/example/vgalery/VgaleryApplication.java
+++ src/main/java/com/example/vgalery/VgaleryApplication.java
@@ -3,12 +3,20 @@ package com.example.vgalery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RestController
 public class VgaleryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VgaleryApplication.class, args);
+	}
+
+	@GetMapping("/_status")
+	public String getStatus() {
+		return "Ok";
 	}
 }
