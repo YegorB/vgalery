3f1248fa33d5ebb1b9077e60b0b4dc09f0a9f748
Yegor Budnichenko <y.budnichenko@gmail.com>
2018-12-22 23:43:36 +0200

    fixed issue with 403

--- src/main/java/com/example/vgalery/controller/PaintingController.java
+++ src/main/java/com/example/vgalery/controller/PaintingController.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.Random;
 
 @RestController
-@CrossOrigin(origins = {"https://vgallery-ui.appspot.com", "http://localhost:3000"})
+@CrossOrigin(origins = {"https://vgallery-ui.appspot.com", "http://vgallery-ui.appspot.com", "http://localhost:3000"})
 public class PaintingController {
 
 /*
